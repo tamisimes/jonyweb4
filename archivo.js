b6e4fab67d74d86c4eb35856d5507193c34cd9bf
Tami <tamisimes2@gmail.com>
2024-01-03 10:25:55 -0300

    agregué logos de las univ y de los trabajos, agrandé nav

--- archivo.js
+++ archivo.js
@@ -24,7 +24,28 @@ $(document).ready(function() {
             rect.right <= (window.innerWidth || document.documentElement.clientWidth)
         );
     }
+
+    // Agrega un evento scroll adicional para detectar cuando el usuario pasa por la sección
+    $(window).on('scroll', function() {
+        $('.seccion2, .seccion3').each(function() {
+            if (elementInViewport(this) && !$(this).hasClass('mostrar')) {
+                $(this).trigger('inViewport');
+            }
+        });
+    });
+
+    // Agrega la animación cuando el usuario pasa por la sección
+    $('.seccion2, .seccion3').on('inViewport', function() {
+        $(this).addClass('mostrar');
+        $(this).find('.carta-animada, .carta-experiencia').each(function(index) {
+            var carta = $(this);
+            setTimeout(function() {
+                carta.addClass('mostrar');
+            }, 300 * index); // Ajusta la velocidad de la transición aquí
+        });
+    });
 });
+
 
 
 $(document).ready(function() {
